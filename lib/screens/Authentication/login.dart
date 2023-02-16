28487d672c1789f5beed248c91fa04b06d324b21
Nightfalls-Xmachine <emmanuel.erhabor98@gmail.com>
2023-02-16 06:38:06 -0800

    Provider with Rest API

--- lib/screens/Authentication/login.dart
+++ lib/screens/Authentication/login.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider_todo_list/screens/Authentication/register.dart';
+import 'package:provider_todo_list/widgets/button.dart';
+
+import '../../utils/routers.dart';
+import '../../widgets/text_field.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -8,8 +13,57 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _email = TextEditingController();
+  final TextEditingController _password = TextEditingController();
+
+  @override
+  void dispose() {
+    _email.clear();
+    _password.clear();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Login ')),
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Container(
+              padding: const EdgeInsets.all(15),
+              child: Column(
+                children: [
+                  customTextField(
+                    title: 'Email',
+                    controller: _email,
+                    hint: 'Enter you valid email address',
+                  ),
+                  customTextField(
+                    title: 'Password',
+                    controller: _password,
+                    hint: 'Enter your secured password',
+                  ),
+                  customButton(
+                    text: 'Login',
+                    tap: (){},
+                    context: context,
+                    status: false
+                  ),
+                  const SizedBox(height: 10,),
+                  GestureDetector(
+                    onTap: () {
+                      PageNavigator(ctx: context)
+                          .nextPage(page: const RegisterPage());
+                    },
+                    child: const Text('Register Instead'),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/screens/Authentication/register.dart
+++ lib/screens/Authentication/register.dart
@@ -1,1 +1,61 @@
- 
+import 'package:flutter/material.dart';
+import 'package:provider_todo_list/widgets/button.dart';
+
+import '../../utils/routers.dart';
+import '../../widgets/text_field.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({Key? key}) : super(key: key);
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final TextEditingController _email = TextEditingController();
+  final TextEditingController _password = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Login ')),
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Container(
+              padding: const EdgeInsets.all(15),
+              child: Column(
+                children: [
+                  customTextField(
+                    title: 'Email',
+                    controller: _email,
+                    hint: 'Enter you valid email address',
+                  ),
+                  customTextField(
+                    title: 'Password',
+                    controller: _password,
+                    hint: 'Enter your secured password',
+                  ),
+                  customButton(
+                      text: 'Login',
+                      tap: (){},
+                      context: context,
+                      status: false
+                  ),
+                  const SizedBox(height: 10,),
+                  GestureDetector(
+                    onTap: () {
+                      PageNavigator(ctx: context)
+                          .nextPage(page: const RegisterPage());
+                    },
+                    child: const Text('Register Instead'),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
